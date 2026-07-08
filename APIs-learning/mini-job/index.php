24bd47fb55b9a0875cb372cbd26401d2125284f1
Quiel <ing.jeremy.quiel@gmail.com>
2026-07-08 12:26:33 -0600

    mini-job API beta v1.1 (GET finished)

--- APIs-learning/mini-job/index.php
+++ APIs-learning/mini-job/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$movie = [
+$movies = [
   ["id" => 1, "title" => "El Padrino", "director" => "Francis Ford", "date" => 1972, "genre" => "drama", "rate" => 9.2],
   ["id" => 2, "title" => "El Perro", "director" => "Jeremy Salas", "date" => 2026, "genre" => "suspenso", "rate" => 10],
   ["id" => 3, "title" => "GOT", "director" => "Maria Isabel", "date" => 2000, "genre" => "fantasia", "rate" => 7.8]
@@ -13,13 +13,44 @@ $method = $_SERVER['REQUEST_METHOD']; # Obtiene el metodo (GET, POST, PUT, DELET
 
 $route = explode('/', trim($_SERVER['REQUEST_URI'], '/')); # array con la ruta dividia por indices
 
-$source = $route[0];
+$source = $route[0] ?? '';
+$sec = $route[1] ?? '';
 
-if ($source === "movies"){
-  
+if (str_starts_with($source, "movies")){
+
   if ($method === "GET"){
-    http_response_code(200);
-    echo json_encode(["status" => "success", "message" => "GET Success"]);
+    $sort = $_GET['sort'] ?? '';
+    $dir = $_GET['dir'] ?? '';
+
+    if ($sec === ''){
+      if ($sort === '' and $dir === '') {
+        http_response_code(200);
+        echo json_encode(["status" => "success", "message" => $movies]);
+        exit;
+      }
+
+      $sort_movies = sort_movies($movies, $sort, $dir);
+      
+      if ($sort_movies === ''){
+        http_response_code(400);
+        echo json_encode(["status" => "error", "message" => "can not sort the movies"]);
+        exit;
+      }
+
+      http_response_code(200);
+      echo json_encode(["status" => "success", "message" => $sort_movies]);
+      exit;
+    }
+
+    foreach ($movies as &$movie){
+      if ($movie['id'] == $sec){
+        http_response_code(200);
+        echo json_encode(["status" => "success", "message" => $movie]);
+        exit;
+      }
+    }
+    http_response_code(404);
+    echo json_encode(["status" => "error", "message" => "Movie not found"]);
     exit;
   }
 
@@ -43,4 +74,49 @@ if ($source === "movies"){
 
 http_response_code(404);
 echo json_encode(["status" => "error", "message" => "endpoint not found, check your address or run the server correctly"]);
+?>
+
+
+
+
+
+<?php
+
+function sort_movies($movies, $sort, $dir){
+  $valid_sort = ["date", "rate"];
+  $valid_dir = ["asc", "desc"];
+  if (!in_array($sort, $valid_sort)){
+    return '';
+  }
+  if (!in_array($dir, $valid_dir)){
+    return '';
+  }
+
+  $final_movies = [];
+
+  $sort_movies = [];
+
+  foreach ($movies as $movie){
+    $sort_movies[] = $movie[$sort];
+  }
+
+  if ($dir === "desc"){
+    rsort($sort_movies);
+  }
+  else{
+    sort($sort_movies);
+  }
+
+  foreach ($sort_movies as $num){
+    foreach ($movies as $movie){
+      if ($movie[$sort] == $num){
+        $final_movies[] = $movie;
+        break;
+      }
+    }
+  }
+
+  return $final_movies;
+}
+
 ?>
